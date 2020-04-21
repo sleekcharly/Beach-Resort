bbea938df3a56b179c300f44f052099f01b4730e
sleekcharly <c_ukasoanya@yahoo.com>
2020-04-21 06:55:44 +0100

    pre-deployment commit

--- src/Contentful.js
+++ src/Contentful.js
@@ -1,6 +1,6 @@
 import { createClient } from 'contentful';
 
 export default createClient({
-    space: "05ekpe7y2uvn",
-    accessToken: "yxpyIPqEIlsu45-b-MR6lEuQnAIzRXXZGra7dssUEEU"
+    space: process.env.REACT_APP_API_SPACE,
+    accessToken: process.env.REACT_APP_ACCESS_TOKEN
 });
